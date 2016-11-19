27ed11dada1d49ea1d64aaede81df593c578e27b
Jeremy Yen <jeremy5189@gmail.com>
2016-11-19 16:34:34 +0800

    Some changes

--- js/controllers.js
+++ js/controllers.js
@@ -3,7 +3,21 @@ angular.module('app.controllers', [])
 // You can include any angular dependencies as parameters for this function
 // TIP: Access Route Parameters for your page via $stateParams.parameterName
 function ($scope, $stateParams) {
+  $scope.next = function ($event) {
 
+    let amount = document.getElementById('pay-amount').value,
+        country = document.getElementById('pay-country').value,
+        comm = document.getElementById('pay-commission').value;
+
+    let data = {
+      amount: amount,
+      country: country,
+      commission: comm
+    };
+
+    localStorage.setItem('data', JSON.stringify(data));
+    // console.log(JSON.parse(localStorage.getItem('data')).amount);
+  };
 }])
 .controller('historyCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
 // You can include any angular dependencies as parameters for this function
