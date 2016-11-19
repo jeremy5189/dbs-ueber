c989c89f7de4e2ff36747307c7e17533ec97f572
zazaman <a28283878@gmail.com>
2016-11-19 16:48:40 +0800

    KKK

--- js/controllers.js
+++ js/controllers.js
@@ -3,6 +3,33 @@ angular.module('app.controllers', [])
 // You can include any angular dependencies as parameters for this function
 // TIP: Access Route Parameters for your page via $stateParams.parameterName
 function ($scope, $stateParams) {
+  let contact = {
+    "Jeremy Yen": {
+        "Picture" : "img/mic73kYRGmZ8uiiAFrfw_2016-11-191.31.16.png",
+        "Passport Name" : "Jeremy Yen";
+        "Email" : "Jeremy@gmail.com";
+        "Payment Type" : "DBS Bank Acc";
+        "Account Number" : "60200012345";
+        "Note" : "";
+    },
+    "Issac Chen": {
+        "Picture" : "img/TInAxhrQh6Tt7BYTIquQ_2016-11-191.31.29.png",
+        "Passport Name" : "Issac Chen";
+        "Email" : "Issac@gmail.com";
+        "Payment Type" : "DBS Bank Acc";
+        "Account Number" : "60200054321";
+        "Note" : "";
+    },
+    "Richard Lin": {
+        "Picture" : "img/O1nvMW1RaikI01gv6AcQ_2016-11-191.31.41.png",
+        "Passport Name" : "Richard Lin";
+        "Email" : "Richard@gmail.com";
+        "Payment Type" : "DBS Bank Acc";
+        "Account Number" : "60200078078";
+        "Note" : "";
+    }
+  };
+
   $scope.next = function ($event) {
 
     let amount = document.getElementById('pay-amount').value,
@@ -16,6 +43,7 @@ function ($scope, $stateParams) {
     };
 
     localStorage.setItem('data', JSON.stringify(data));
+    localStorage.setItem('contact', JSON.stringify(contact));
     // console.log(JSON.parse(localStorage.getItem('data')).amount);
   };
 }])
@@ -35,7 +63,7 @@ function ($scope, $stateParams) {
 // You can include any angular dependencies as parameters for this function
 // TIP: Access Route Parameters for your page via $stateParams.parameterName
 function ($scope, $stateParams) {
-
+  let contacts = localStorage.getItem('contact');
 }])
 .controller('dBSBerCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
 // You can include any angular dependencies as parameters for this function
